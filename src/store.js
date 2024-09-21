933734689a8c7d71028f779124de216402f8e06e
sono <rhj080471@gmail.com>
2024-09-21 22:07:36 +0900

    sem1

--- src/store.js
+++ src/store.js
@@ -1,1 +1,26 @@
-//
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+const reducers = combineReducers({
+  user: userReducer,
+  darkMode: darkModeReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export default store;
