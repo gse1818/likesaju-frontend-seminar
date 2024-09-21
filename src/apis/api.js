023b2eff3a754d14f67a1bbc928c5c0cc3c80156
boxyhn <parksh937@naver.com>
2024-09-21 14:27:27 +0900

    feat: kakao-login

--- src/apis/api.js
+++ src/apis/api.js
@@ -40,17 +40,7 @@ export const signOut = async () => {
 };
 
 export const kakaoSignIn = async (data) => {
-  try {
-    const response = await instance.post(
-      '/user/kakao/callback/?code=' + data.code,
-    );
-    if (response.status === 200) {
-      return response.data;
-    }
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
+
 };
 
 export const fetchUserInfo = async () => {
