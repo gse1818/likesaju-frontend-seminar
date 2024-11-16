75ea21646e4f01977138758325ed53022890e4eb
suhan8984 <suhan8984@daum.net>
2024-11-16 15:36:16 +0900

    seminar

--- src/routes/main/components/faq-section.jsx
+++ src/routes/main/components/faq-section.jsx
@@ -47,38 +47,36 @@ const FAQAccordion = ({ question, answer }) => {
       <div className="flex justify-between items-center gap-5">
         <p className="text-xl font-bold truncate">{question}</p>
         <button
-          className="rounded-full shadow-lg transition"
+          className="rounded-full shadow-md transition border border-[#D3D3D3]"
           onClick={() => {
             setIsOpen(!isOpen);
           }}
         >
           <svg
-            className={`transition transform ${isOpen ? '' : '-rotate-90'}`}
+            className={`transition-transform transform ${
+              isOpen ? 'rotate-180' : 'rotate-0'
+            }`}
             xmlns="http://www.w3.org/2000/svg"
-            width="51"
-            height="51"
-            viewBox="0 0 51 51"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
             fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
-            <circle
-              className="transition"
-              cx="25.6691"
-              cy="25.3309"
-              r="25.3309"
-              fill={!isOpen ? '#FFFFFF' : '#6F6C90'}
-            />
-            <path
-              className="transition"
-              d="M17.4125 22.2212L25.6691 30.4405L33.9257 22.2212"
-              stroke={!isOpen ? '#6F6C90' : '#FFFFFF'}
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
+            <polyline points="6 9 12 15 18 9"></polyline>
           </svg>
         </button>
       </div>
-      {isOpen && <p className="text-lg w-full text-left">{answer}</p>}
+      <div
+        className={`transition-max-height duration-1000 ease-in-out overflow-hidden ${
+          isOpen ? 'max-h-screen' : 'max-h-0'
+        }`}
+      >
+        <p className="text-lg w-full text-left mt-4">{answer}</p>
+      </div>
     </div>
   );
 };
